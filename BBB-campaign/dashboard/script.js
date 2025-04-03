187f0c4830284b23ee111b8911e4ec37b4873c88
YTseen <justsignmein1@gmail.com>
2025-04-03 13:15:20 +0200

    Update script.js

--- BBB-campaign/dashboard/script.js
+++ BBB-campaign/dashboard/script.js
@@ -4,7 +4,7 @@ let selectedKey = "";
 
 async function loadQuests() {
   try {
-    const res = await fetch("https://ytseen.github.io/bo1-bloodbath-bbb/BBB-campaign/quest_data.json");
+    const res = await fetch("https://raw.githubusercontent.com/YTseen/bo1-bloodbath-bbb/refs/heads/main/BBB-campaign/quest_data.json");
     questData = await res.json();
     const questList = document.getElementById("questList");
     questList.innerHTML = "";
